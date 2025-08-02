81fa688e66c4ef4e650ef9a9a279ea7ebb197b75
stse416 <43531621+stse416@users.noreply.github.com>
2025-08-01 23:02:27 -0400

    Move black background from body to rowContainer class

--- javascript.js
+++ javascript.js
@@ -3,20 +3,20 @@ function createPad(rowContainer) {
     rowContainer.className = 'rowContainer';
     while (rowContainer.childElementCount < 16) {
         rowContainer.appendChild(document.createElement('div'));
-}
-
-[...rowContainer.children].forEach((row) => {
-    while(row.childElementCount < 16) {
-        let etchSquare = document.createElement('div');
-
-        etchSquare.addEventListener('mouseenter', () => {
-            etchSquare.className = 'etched';
-            etchSquare.style.opacity = String(Number(getComputedStyle(etchSquare).opacity) - .1);
-        })
-
-        row.appendChild(etchSquare);
     }
-}) 
+
+    [...rowContainer.children].forEach((row) => {
+        while(row.childElementCount < 16) {
+            let etchSquare = document.createElement('div');
+
+            etchSquare.addEventListener('mouseenter', () => {
+                etchSquare.className = 'etched';
+                etchSquare.style.opacity = String(Number(getComputedStyle(etchSquare).opacity) - .1);
+            })
+
+            row.appendChild(etchSquare);
+        }
+    }) 
 }
 
 createPad();
