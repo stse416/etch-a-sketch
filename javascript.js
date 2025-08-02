5676004366e846d9af1b1087c926cc8bdb3bcd0f
stse416 <43531621+stse416@users.noreply.github.com>
2025-08-02 01:10:44 -0400

    Change etchPad divs to be squares. Will no longer utilize whole window

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
+const resetButton = document.querySelector('button');
+resetButton.addEventListener('mouseup', () => resetPad())
+let buttonHeight = getComputedStyle(resetButton).height.substring(0, 2);
+
+let maxSize = 0;
+let usableHeight = innerHeight - buttonHeight;
+
+if (usableHeight > innerWidth) maxSize = innerWidth
+else maxSize = usableHeight;
+
+
 function createPad(rowContainer, size) {
     if (size > 100 || size < 0 || size.length == 0 ) size = 100;
 
     rowContainer = document.querySelector('div');
     rowContainer.className = 'rowContainer';
+    rowContainer.style.height = `${maxSize}px`;
+    rowContainer.style.width = `${maxSize}px`;
+
     while (rowContainer.childElementCount < size) {
         rowContainer.appendChild(document.createElement('div'));
     }
@@ -23,12 +37,10 @@ function createPad(rowContainer, size) {
 
 createPad(document.querySelector('div'), 50);
 
+
 function resetPad() {
     let newRowContainer = document.createElement('div');
     document.querySelector('div').replaceWith(newRowContainer);
     createPad(newRowContainer, 
         prompt('What size grid would you like to sketch on? Max value is 100 to create a 100x100 grid'));
 }
-
-const resetButton = document.querySelector('button');
-resetButton.addEventListener('mouseup', () => resetPad())
